96fcd16a4b700f26f319396f92d72c95bb86f2d0
GregBasera <basera.gg@gmail.com>
2021-01-17 16:23:19 +0800

    tried docker-compose; works; added react-router-dom

--- site/src/App.js
+++ site/src/App.js
@@ -1,8 +1,20 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Auth from "./components/auth/Auth";
+import Home from "./components/home/Home";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">react</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path={["/", "/auth"]} exact component={Auth} />
+        <Route path="/home" exact component={Home} />
+        <Route component={NotFound} /> {/* 404 */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ site/src/components/auth/Auth.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Auth() {
+  return <div>this is auth</div>;
+}
