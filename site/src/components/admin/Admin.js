05ea65f359e442fbd92f4799a97d20b8b7194d88
GregBasera <basera.gg@gmail.com>
2021-03-25 15:15:03 +0800

    decided not to pursue stats, too much work in strapi

--- site/src/components/admin/Admin.js
+++ site/src/components/admin/Admin.js
@@ -2,8 +2,6 @@ import React from "react";
 import AppNavBar from "../AppNavBar";
 import { Redirect } from "react-router-dom";
 import PageBody from "./PageBody";
-import Graph from "./statistics/Graph";
-import { Container } from "@material-ui/core";
 
 export default function Admin() {
   const auth = JSON.parse(sessionStorage.getItem("auth"));
@@ -14,24 +12,6 @@ export default function Admin() {
       <AppNavBar />
 
       <PageBody />
-
-      <Container>
-        <Graph
-          // maxHeight="500px"
-          type="pie"
-          data={{
-            labels: ["Red", "Blue", "Yellow"],
-            datasets: [
-              {
-                // label: ["dummy data", "dummy data", "dummy data"],
-                data: [12, 19, 3],
-                backgroundColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
-              },
-            ],
-          }}
-          options={null}
-        />
-      </Container>
     </div>
   );
 }
